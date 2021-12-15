16dd76d5a132c0385fbbbd295600125d8d8590bf
stefanlliu <stefanlliu@tencent.com>
2021-12-15 22:16:46 +0800

    feat：配置less-loader

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './style.less'
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className="test">
+        <h1 className="halo">This is React App.</h1>
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default App
